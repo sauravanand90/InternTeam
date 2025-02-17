fffae6433c99aa33e93a827dbead7ee4c19fbc68
Lalitha Shree C P <113790298+Lalli17@users.noreply.github.com>
2025-02-17 15:01:33 +0530

    Update loops_pgm.js

--- loops_pgm.js
+++ loops_pgm.js
@@ -1,3 +1,6 @@
+/*JavaScript program that iterates integers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". 
+For numbers multiples of both three and five print "FizzBuzz".*/
 // fizz buzz
 for(var i=0;i<=100;i++)
 {
