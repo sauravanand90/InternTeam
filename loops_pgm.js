088891223ef84bd202513189ec3e2c4a6dbe3137
Lalitha Shree C P <113790298+Lalli17@users.noreply.github.com>
2025-02-17 14:56:34 +0530

    Add files via upload
    updated program

---
+++ loops_pgm.js
@@ -0,0 +1,12 @@
+// fizz buzz
+for(var i=0;i<=100;i++)
+{
+    if(i%3==0 && i%5==0)
+        console.log(i+ " FizzBuzz");
+    else if(i%3==0)
+        console.log(i+ " Fizz");
+    else if(i%5==0)
+        console.log(i+ " Buzz");
+    else
+    console.log(i);
+}
