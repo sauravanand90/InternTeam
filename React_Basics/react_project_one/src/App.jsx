02f98155d91ff311c4e3fa91657ccfb3c15713a0
Maurya <aanchal.maurya@harman.com>
2025-03-07 13:01:47 +0530

    React_Basics

---
+++ React_Basics/react_project_one/src/App.jsx
@@ -0,0 +1,23 @@
+import UserCard from "./components/UserCard"
+import Lifecycle from "./components/lifecycle";
+import Updating from "./components/lifecycle";
+import UseState from "./components/UseState";
+
+// import "./App.css"
+// import pic2 from "./assets/pic2.jpg"
+// import pic3 from "./assets/pic3.jpg"
+// import pic4 from "./assets/pic4.jpg"
+
+function App() {
+  return (
+    <div className="container">
+      {/* <UserCard name="Aditi Shrinet" desc="Works as Software Developer in Delhi" image={pic2} style={{"border-radius":"10px"}}/>
+      <UserCard name="Sneha Mishra" desc="Works as Data Analyst in Mumbai" image={pic3} style={{"border-radius":"10px"}}/>
+      <UserCard name="Mansi Singh" desc="Works as Full Stack Developer in Pune" image={pic4} style={{"border-radius":"10px"}}/> */}
+      {/* <Lifecycle /> */}
+      {/* <Updating />  */}
+      <UseState />
+    </div>
+  )
+}
+export default App;
---
+++ React_Basics/react_project_one/src/components/UserCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import pic2 from '../assets/pic2.jpg'
+import "./UserCard.css"
+const UserCard = (props) => {
+  return (
+    <div className='user-container' style={props.style}>
+        <p id='user-name'>{props.name}</p>
+        <img id='user-image' src={props.image} alt={props.name}></img>
+        <p id='user-desc'>{props.desc}</p>
+    </div>
+  )
+}
+
+export default UserCard
